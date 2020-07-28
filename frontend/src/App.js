0517d56b3cb3325a8704d31951fcfe1627bd24bf
kevintu <kgtu154@gmail.com>
2020-07-28 18:52:41 -0400

    now able to handle multiple tags

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,8 +23,9 @@ class App extends Component {
       );
   }*/
 
-  getTagQuote = () => {
-    const url = "http://127.0.0.1:8000/tag/?type=love";
+  getTagQuote = (e) => {
+    //console.log(e);
+    const url = "http://127.0.0.1:8000/tag/?type=" + e;
     fetch(url)
       .then(resp => resp.json())
       .then(data => 
@@ -61,7 +62,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Based On Tags</h1>
-        <button onClick={this.getTagQuote}>Love</button>
+        <button onClick={() => this.getTagQuote("hope")}>Hope</button>
+        <button onClick={() => this.getTagQuote("joy")}>Joy</button>
+        <button onClick={() => this.getTagQuote("sad")}>Sad</button>
         <h1>Randomizer</h1>
         <button onClick={this.getRandomQuote}>Random Quote</button>
         <div style={{ padding:"100px" }}>
